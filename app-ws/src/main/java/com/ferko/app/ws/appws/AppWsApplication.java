d27d5bc28bdf5aaebde240f307a6b06a82815b8a
Firealem Erko <firealemg@yahoo.com>
2019-07-14 16:46:24 -0400

    project structure setup

---
+++ app-ws/src/main/java/com/ferko/app/ws/appws/AppWsApplication.java
@@ -0,0 +1,15 @@
+package com.ferko.app.ws.appws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWsApplication
+{
+//test
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AppWsApplication.class, args);
+    }
+
+}
